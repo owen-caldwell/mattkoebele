7b437a508ec31ef895c061e013969d5ae7fe6e3a
Owen Caldwell <caldwellom@gmail.com>
2025-07-24 16:40:23 -0400

    followups july 24

--- expand.js
+++ expand.js
@@ -5,34 +5,32 @@ const dfilmwrap = document.querySelector(".dfilm-wrapper");
 const mArrow = mobileMain.querySelector("img");
 const dArrow = desktopMain.querySelector("img");
 let mobileExpand = true;
-let desktopExpand = false
+let desktopExpand = false;
 mobileMain.addEventListener("click", () => {
-    if (mobileExpand === false) {
-        mobileExpand = true;
-        console.log("opening");
-        mArrow.style.transform = "rotate(180deg)"
-        mfilmwrap.style.height = "220px";
-        mfilmwrap.style.marginBottom = "2em";
-
-    } else {
-        mobileExpand = false;
-        console.log("closing");
-        mArrow.style.transform = "rotate(0deg)"
-        mfilmwrap.style.height = "0px";
-        mfilmwrap.style.marginBottom = "0";
-    }
+  if (mobileExpand === false) {
+    mobileExpand = true;
+    console.log("opening");
+    mArrow.style.transform = "rotate(180deg)";
+    mfilmwrap.style.height = "260px";
+    mfilmwrap.style.marginBottom = "2em";
+  } else {
+    mobileExpand = false;
+    console.log("closing");
+    mArrow.style.transform = "rotate(0deg)";
+    mfilmwrap.style.height = "0px";
+    mfilmwrap.style.marginBottom = "0";
+  }
 });
 desktopMain.addEventListener("click", () => {
-    if (desktopExpand === false) {
-        desktopExpand = true;
-        console.log("opening");
-        dArrow.style.transform = "rotate(0deg)"
-        dfilmwrap.style.height = "220px";
-
-    } else {
-        desktopExpand = false;
-        console.log("closing");
-        dArrow.style.transform = "rotate(180deg)"
-        dfilmwrap.style.height = "0px";
-    }
+  if (desktopExpand === false) {
+    desktopExpand = true;
+    console.log("opening");
+    dArrow.style.transform = "rotate(0deg)";
+    dfilmwrap.style.height = "260px";
+  } else {
+    desktopExpand = false;
+    console.log("closing");
+    dArrow.style.transform = "rotate(180deg)";
+    dfilmwrap.style.height = "0px";
+  }
 });
